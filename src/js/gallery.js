4a0da6b8ce9522ad6c1a7a44e3fa86ec961bfdcf
Korotkova-Iuliia <3589900@gmail.com>
2021-11-17 09:41:29 +0200

    key

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -12,6 +12,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 //   else console.log('No hits');
 // });
 // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// key - твой уникальный ключ доступа к API.
+// q - термин для поиска. То, что будет вводить пользователь.
+// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
+// orientation - ориентация фотографии. Задай значение horizontal.
+//     safesearch - фильтр по возрасту.Задай значение true.
 
 const API_KEY = `24377768-1651c24dae1d00899e27f41ae`;
 const URL = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent('red roses');
