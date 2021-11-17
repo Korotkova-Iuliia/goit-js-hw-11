2b373e29a0e92f8cec17877c97009e4556030a3e
Korotkova-Iuliia <3589900@gmail.com>
2021-11-18 00:04:26 +0200

    reset

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,12 @@
 const API_KEY = `24377768-1651c24dae1d00899e27f41ae`;
 const BASE_URL = `https://pixabay.com/api`;
-export function fetchTag(tags, page) {
-  const URL = `${BASE_URL}/?key=${API_KEY}&q=${tags}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=4`;
+export function fetchTag(tags, page, perPage) {
+  const URL = `${BASE_URL}/?key=${API_KEY}&q=${tags}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   return fetch(URL).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    console.log(response);
     return response.json();
   });
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -22,34 +22,46 @@
 import { fetchTag } from './api';
 const refs = {
   searchForm: document.querySelector('#search-form'),
-  //   input: document.querySelector('.searchQuery'),
   galleryList: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
+  inputEl: document.querySelector('.searchQuery'),
+  perPage: 40,
 };
+
 let tags = 0;
 let page = 1;
+
 refs.loadMoreBtn.classList.add('is-hidden');
+
 refs.loadMoreBtn.addEventListener('click', () => {
-  fetchTag(tags, page).then(photos => {
+  fetchTag(tags, page, refs.perPage).then(photos => {
     renderPhotos(photos);
     page += 1;
     console.log(photos.total);
-    console.log(photos.totalHits);
-    console.log(page > photos.total / 4);
-    if (page > photos.total / 4) {
+    console.log(page > photos.totalHits / refs.perPage);
+    if (page > photos.totalHits / refs.perPage) {
       refs.loadMoreBtn.classList.add('is-hidden');
     }
   });
 });
-
+refs.searchForm.addEventListener('input', e => {
+  if (e.target !== 0) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return reset();
+  }
+});
 refs.searchForm.addEventListener('submit', e => {
   e.preventDefault();
 
   tags = refs.searchForm.elements.searchQuery.value;
   refs.searchForm.reset();
+
   fetchTag(tags, page).then(photos => {
     renderPhotos(photos);
     page += 1;
+    if (page > photos.totalHits / refs.perPage) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+    }
     refs.loadMoreBtn.classList.remove('is-hidden');
   });
 });
@@ -88,10 +100,12 @@ function renderPhotos({ hits }) {
   //  <img class="large-img" src="${largeImageURL}" alt="${tags}" weight="20" loading="lazy" />;
   refs.galleryList.insertAdjacentHTML('beforeend', markup);
 }
-// galleryListReset();
-// function galleryListReset() {
-//   galleryList.innerHTML = '';
-// }
+
+function reset() {
+  console.log('сброс');
+
+  refs.galleryList.innerHTML = '';
+}
 // ${hit.previewURL}
 // function notifyInfo() {
 //   Notify.info('Too many matches found. Please enter a more specific name.', {
